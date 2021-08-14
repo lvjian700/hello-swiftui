c87e73a2cf0dec8af0ba6a2eec15560a7fd57d89
Jian Lv <lvjian700@gmail.com>
2021-08-14 10:20:07 +1000

    Initial Commit

---
+++ hello-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello-swiftui
+//
+//  Created by Jian Lyu on 14/8/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
